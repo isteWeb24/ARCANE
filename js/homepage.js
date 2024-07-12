b249823db537295cf89797b776433c9134cc76b2
unknown <abhinavkb2004@gmail.com>
2024-07-12 19:58:15 +0530

    first image and its content

--- js/homepage.js
+++ js/homepage.js
@@ -1,7 +1,14 @@
+/*Hamburger menu */
+
 const ham_button = document.querySelector('.hamburger');
 const ham_menu = document.querySelector('.ham-list');
 
 ham_button.addEventListener('click', function(){
     ham_button.classList.toggle('is-active');
     ham_menu.classList.toggle('is-active');
-})
+});
+
+
+
+
+/* */
