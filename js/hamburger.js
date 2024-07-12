70be68f6328b7e9fff612e4587e8ce5e961a805b
unknown <abhinavkb2004@gmail.com>
2024-07-12 10:34:13 +0530

    navbar and ham menu

---
+++ js/hamburger.js
@@ -0,0 +1,7 @@
+const ham_button = document.querySelector('.hamburger');
+const ham_menu = document.querySelector('.ham-list');
+
+ham_button.addEventListener('click', function(){
+    ham_button.classList.toggle('is-active');
+    ham_menu.classList.toggle('is-active');
+})
